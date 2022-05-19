2eebbdc36bc91bd54282ec69953354077f14879b
Ricardo Victor <rvictorsoliveira@gmail.com>
2022-05-19 14:54:01 -0300

    add reuse

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
+    <Reuse />
     <h1>Hello Vue</h1>
     <p>Creator: {{ name }}, {{ occupation }}</p>
+    <Reuse />
     <MultipleEvents />
+    <Reuse />
   </div>
 </template>
 
 <script>
 import MultipleEvents from "./MultipleEvents.vue";
+import Reuse from './Reuse.vue';
 export default {
   components: {
     MultipleEvents,
+    Reuse,
   },
   name: "MainComponent",
   data() {
---
+++ src/components/Reuse.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="activate">Clique aqui para mudar ativação independente!</button>
+    <span v-show="active">Ativado!</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Reuse",
+  data() {
+    return {
+      active: false,
+    };
+  },
+  methods: {
+    activate() {
+      this.active = !this.active;
+    },
+  },
+};
+</script>
